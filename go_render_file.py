d3fe27409eee189e0524e6ab6b1d652a727381e6
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-10-07 14:35:51 +0300

    new class to open view files

---
+++ go_render_file.py
@@ -0,0 +1,35 @@
+import os, sublime, sublime_plugin
+
+class GoRenderFileCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    region    = self.view.sel()[0]
+    line      = self.view.line(region)
+    line_text = self.view.substr(line)
+
+    if '"' in line_text:
+      file_name = line_text.split('"')[1]
+    else:
+      file_name = line_text.split('\'')[1]
+
+    if len(file_name) > 0:
+      source          = self.view.file_name()
+      source_path     = os.path.dirname(source)
+      rails_view_path = os.path.dirname(source_path)
+
+      if '/' in file_name:
+        split_file_name = file_name.split('/')
+        if len(split_file_name) == 2:
+          new_file_name = split_file_name[0] + '/_' + split_file_name[1]
+        else:
+          new_file_name = split_file_name[0] + '/' + split_file_name[1] + '/_' + split_file_name[2]
+
+        file_path = rails_view_path + '/' + new_file_name
+      else:
+        new_file_name = file_name
+        file_path = source_path + '/_' + new_file_name
+
+      extensions = ['haml', 'html.erb', 'html.slim']
+
+      for i in extensions:
+        if os.path.exists(file_path + '.' + i):
+          self.view.window().open_file(file_path + '.' + i)
